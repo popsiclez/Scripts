6adb2cd7a8ae6612ac9554a14677845697414eb6
popsiclez <74466666+popsiclez@users.noreply.github.com>
2021-08-30 12:08:44 -0400

    Create ScriptHub.lua

---
+++ ScriptHub.lua
@@ -0,0 +1,14 @@
+if workspace:FindFirstChild("Water") ~= nil then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/popsiclez/Scripts/main/BABFTv2.lua", true))()
+print("Game Found (BABFT)")
+end
+
+if workspace:FindFirstChild("FountainKnife") ~= nil then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/popsiclez/Scripts/main/AssassinVynixu.lua", true))()
+print("Game Found (Assassin)") 
+end
+
+if workspace:FindFirstChild("DeadTree") ~= nil then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/popsiclez/Scripts/main/BrokenBonesIV", true))()
+print("Game Found (Broken Bones IV")    
+end
